66bde662f4d8363454df3b04fabdf8e850bc8f22
nathrussellg <nathrussellg@users.noreply.github.com>
2020-10-24 14:45:02 -0700

    1024 Fixes 1
    Fixed missing colon in ssg and updated dir variable in site class

--- ssg.py
+++ ssg.py
@@ -1,9 +1,11 @@
 import typer
 from ssg.site import Site
 
-def main(source="content", dest="dist")
-    config = {"source":source, "dest":dest}
+
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest}
     s1 = Site(**config)
     s1.build()
+
 
 typer.run(main)
