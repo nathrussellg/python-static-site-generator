58621cfd5871a123b909769bb37435b63aa29ff6
ngustaf <nathan.gustafson@nextiva.com>
2020-10-28 16:50:32 -0700

    Test

--- ssg/content.py
+++ ssg/content.py
@@ -4,17 +4,18 @@ from collections.abc import Mapping
 
 class Content(Mapping):
 
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
     @classmethod
     def load(self, cls, string):
-        _, fm, content = __regex.split(string, 2)
-        load(fm, Loader = FullLoader)
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader = FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
+        self.data["content"] = content
 
     @property
     def body(self):
@@ -22,26 +23,24 @@ class Content(Mapping):
 
     @property
     def type(self):
-        return self.data if "type" else None
+        return self.data["type"] if "type" in self.data else None
 
     @type.setter
-    def type(self, val):
+    def type(self, type):
         type = self.data["type"]
 
-        def __getitem__(self, key):
-            return self.data[key]
+    def __getitem__(self, key):
+        return self.data[key]
 
-        def __iter__(self):
-            # Self.data interator method
+    def __iter__(self):
+        self.data.__iter__()
 
-        def __len__(self):
-            return len(self.data)
+    def __len__(self):
+        return len(self.data)
 
-        def __repr__(self):
-            data = {}
-            for key, value in self.data.items():
-                if key != "content":
-                    data[key] = value
-            return str(data)
-
-    
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
